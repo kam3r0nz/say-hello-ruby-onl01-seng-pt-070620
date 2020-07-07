9c78f6efa66806f5100cb52bac6dccdf1affac83
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-07 16:34:33 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,9 @@
 # Build your say_hello method here
 def say_hello(name)
-  puts "Hello Kent Back!"
+  puts "Hello ${name}"
 end
 
+say_hello("Kent Back")
 
 def say_hello(name = "Ruby Programmer")
   puts "Hello ${name}!"
